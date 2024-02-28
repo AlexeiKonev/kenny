95f5fa9d806748038a70f85621e2cc1e9b22753e
AlexeiKonev <alexkonev777@gmail.com>
2024-02-28 15:05:09 +0300

    add sound manager

--- Assets/Platformer/Scripts/MVVM/StartScreenController.cs
+++ Assets/Platformer/Scripts/MVVM/StartScreenController.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+using System;
 
 namespace Kenny
 {
-    using UnityEngine;
-    using UnityEngine.UI;
-    using UniRx;
-    using UniRx.Triggers;
-    using System;
+    
 
     public class StartScreenController : MonoBehaviour
     {
         public Button startButton;
         public Button settingsButton;
         public Button exitButton;
+        public Button soundButton;
+
+        private ISoundManager soundManager;
 
         void Start()
         {
@@ -22,21 +25,30 @@ namespace Kenny
             startButton.OnClickAsObservable().Subscribe(_ => StartGame()).AddTo(this);
             settingsButton.OnClickAsObservable().Subscribe(_ => OpenSettings()).AddTo(this);
             exitButton.OnClickAsObservable().Subscribe(_ => ExitGame()).AddTo(this);
+
+            // Назначаем обработчик нажатия на кнопку звука
+            soundButton.OnClickAsObservable().Subscribe(_ => ToggleSound()).AddTo(this);
+
+            // Инициализация менеджера звука
+            soundManager = new SoundManager();
         }
 
         void StartGame()
         {
             Debug.Log("Игра начата!");
-            // 
-
+            //  
         }
 
         void OpenSettings()
         {
             Debug.Log("Открыть настройки");
             // 
+        }
 
-
+        void ToggleSound()
+        {
+            bool soundActive = soundManager.ToggleSound();
+            Debug.Log("Звук " + (soundActive ? "включен" : "выключен"));
         }
 
         void ExitGame()
@@ -48,6 +60,24 @@ namespace Kenny
 #else
         Application.Quit();
 #endif
+        }
+    }
+
+    // Интерфейс для менеджера звука
+    public interface ISoundManager
+    {
+        bool ToggleSound();
+    }
+
+    // Реализация менеджера звука
+    public class SoundManager : ISoundManager
+    {
+        private bool soundActive = true;
+
+        public bool ToggleSound()
+        {
+            soundActive = !soundActive;
+            return soundActive;
         }
     }
 
